080fa9e5a3e92c84a64add2358e05f7ab76dd9cb
Ayman <ayman.zein.elbehairy@gmail.com>
2020-06-30 23:42:00 +0200

    spread & rest operator

--- spread & rest operator/index.js
+++ spread & rest operator/index.js
@@ -1,4 +1,5 @@
 // spread used to split up array or object elements
+
 const numbers = [1, 2, 3];
 const nums = [...numbers, 4];
 console.log(nums); // [ 1, 2, 3, 4 ]
@@ -11,6 +12,8 @@ const newPerson = {
   age: 26,
 };
 console.log(newPerson); //{ name: 'Ahmed', age: 26 }
+
 // rest used to merge list of arguments into an array
+
 const myFilter = (...args) => args.filter((el) => el === 1);
 console.log(myFilter(1, 2, 3, 4, 5)); // [1]
